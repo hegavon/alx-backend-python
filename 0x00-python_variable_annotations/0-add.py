3aa3e8f376061b6989865740eb776447c05e1867
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-04 18:11:19 +0100

    Backend Python - Task 0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides a function `add` that takes two floats & returns their sum
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns their sum
+
+    Parameters:
+    a (float): The first float number.
+    b (float): The second float number.
+
+    Returns:
+    float: The sum of the two float numbers.
+    """
+    return a + b
