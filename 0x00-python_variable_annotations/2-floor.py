30beb31d55b4166cc2c731506d583bf8a276896b
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-04 18:29:20 +0100

    Backend Python - Task 2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module provides a function `floor` that takes a float and returns its
+floor value.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the float `n`.
+
+    Parameters:
+    n (float): The number to floor.
+
+    Returns:
+    int: The floor value of `n`.
+    """
+    return math.floor(n)
