ec2cf66640b666d2f9690638442d9cfaff8959d6
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-04 18:36:50 +0100

    Backend Python - Task 4. Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates four variables:
+a, pi, i_understand_annotations, and school.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
