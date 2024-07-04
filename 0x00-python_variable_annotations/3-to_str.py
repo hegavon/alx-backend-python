f081cabe2ba62ba211030ffce62e26067492875e
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-04 18:32:47 +0100

    Backend Python - Task 3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides a function `to_str` that takes a float and returns its
+string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the float `n`.
+
+    Parameters:
+    n (float): The number to convert to a string.
+
+    Returns:
+    str: The string representation of `n`.
+    """
+    return str(n)
